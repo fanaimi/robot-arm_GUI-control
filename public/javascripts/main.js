be7d42cca2c581cd7d32bdec95255bb03101cef7
fanaimi <emiliano.fantasia@gmail.com>
2015-11-14 20:54:47 +0000

    express set up

---
+++ public/javascripts/main.js
@@ -0,0 +1,18 @@
+/**
+ * @name    main.js
+ * @desc    takes care of sliders
+ */
+
+(function(){
+var
+
+
+
+    init = function(){
+        console.log("foo");
+    }// init
+;
+
+window.addEventListener("load", init);
+//$(document).on("ready", init);
+})();
